6af362a8eb72b9d86c24d1ed913d5ac110581d4d
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-04-04 22:41:24 +0700

    🔨: add github api

---
+++ app/src/main/java/id/rosyid/exploregithubusers/network/GithubApi.kt
@@ -0,0 +1,38 @@
+package id.rosyid.exploregithubusers.network
+
+import id.rosyid.exploregithubusers.BuildConfig
+import id.rosyid.exploregithubusers.model.SearchUserResponse
+import id.rosyid.exploregithubusers.model.UserDetailResponse
+import id.rosyid.exploregithubusers.model.UserResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface GithubApi {
+    companion object {
+        const val BASE_URL = "https://api.github.com/"
+        const val TOKEN = BuildConfig.API_KEY
+    }
+
+    @GET("users")
+    @Headers("Authorization: token $TOKEN")
+    suspend fun getUser(): Response<UserResponse>
+
+    @GET("users/{username}")
+    @Headers("Authorization: token $TOKEN")
+    suspend fun getUserDetail(@Path("username") username: String): Response<UserDetailResponse>
+
+    @GET("users/{username}/following")
+    @Headers("Authorization: token $TOKEN")
+    suspend fun getFollowingUser(@Path("username") username: String): Response<ArrayList<UserResponse>>
+
+    @GET("users/{username}/followers")
+    @Headers("Authorization: token $TOKEN")
+    suspend fun getFollowersUser(@Path("username") username: String): Response<ArrayList<UserResponse>>
+
+    @GET("search/users")
+    @Headers("Authorization: token $TOKEN")
+    suspend fun searchUsers(@Query("q") query: String): Response<SearchUserResponse>
+}
