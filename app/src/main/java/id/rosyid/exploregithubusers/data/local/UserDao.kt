ef75931a14cedfa56e4154905cf85100f04ccdbd
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-04-05 00:15:25 +0700

    🔨: add local database and dao

---
+++ app/src/main/java/id/rosyid/exploregithubusers/data/local/UserDao.kt
@@ -0,0 +1,23 @@
+package id.rosyid.exploregithubusers.data.local
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import id.rosyid.exploregithubusers.data.entities.User
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM user")
+    fun getAllUsers(): LiveData<List<User>>
+
+    @Query("SELECT * FROM user WHERE username = :username")
+    fun getUser(username: String): LiveData<User>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(users: List<User>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(user: User)
+}
