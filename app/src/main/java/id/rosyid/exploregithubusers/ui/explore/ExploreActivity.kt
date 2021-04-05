373c6733fb699885f28f0fb0c171b6bf83ffc315
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-04-05 12:57:23 +0700

    🔨: add navigation host

--- app/src/main/java/id/rosyid/exploregithubusers/ui/explore/ExploreActivity.kt
+++ app/src/main/java/id/rosyid/exploregithubusers/ui/explore/ExploreActivity.kt
@@ -2,8 +2,15 @@ package id.rosyid.exploregithubusers.ui.explore
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import dagger.hilt.android.AndroidEntryPoint
+import id.rosyid.exploregithubusers.R
 import id.rosyid.exploregithubusers.databinding.ActivityExploreBinding
 
+@AndroidEntryPoint
 class ExploreActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityExploreBinding
@@ -12,5 +19,12 @@ class ExploreActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityExploreBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
