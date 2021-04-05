fae6eccb8ca5fa974b826b31127b03dadb22a86e
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-04-05 10:45:13 +0700

    🔨: add provides module

--- app/src/main/java/id/rosyid/exploregithubusers/di/AppModule.kt
+++ app/src/main/java/id/rosyid/exploregithubusers/di/AppModule.kt
@@ -1,18 +1,24 @@
 package id.rosyid.exploregithubusers.di
 
+import android.content.Context
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.internal.managers.ApplicationComponentManager
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import id.rosyid.exploregithubusers.data.local.AppDatabase
+import id.rosyid.exploregithubusers.data.local.UserDao
 import id.rosyid.exploregithubusers.data.remote.GithubService
+import id.rosyid.exploregithubusers.data.remote.UserRemoteDataSource
+import id.rosyid.exploregithubusers.data.repository.UserRepository
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
-@InstallIn(ApplicationComponentManager::class)
+@InstallIn(SingletonComponent::class)
 object AppModule {
     @Provides
     @Singleton
@@ -26,4 +32,23 @@ object AppModule {
 
     @Provides
     fun provideGithubService(retrofit: Retrofit): GithubService? = retrofit.create(GithubService::class.java)
+
+    @Singleton
+    @Provides
+    fun provideUserRemoteDataSource(githubService: GithubService) = UserRemoteDataSource(githubService)
+
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext appContext: Context) = AppDatabase.getDatabase(appContext)
+
+    @Singleton
+    @Provides
+    fun provideUserDao(database: AppDatabase) = database.userDao()
+
+    @Singleton
+    @Provides
+    fun provideRepository(
+        remoteDataSource: UserRemoteDataSource,
+        localDataSource: UserDao
+    ) = UserRepository(remoteDataSource, localDataSource)
 }
